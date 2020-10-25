31daeb4584a3c691daef7ff662c33ff94afbbb74
Sy Le <lenguyensy@gmail.com>
2020-10-24 19:33:33 -0700

    Update CacheUtils.ts

--- CacheUtils.ts
+++ CacheUtils.ts
@@ -1,19 +1,25 @@
 import redis from 'redis';
 
+import { getLogger } from 'src/utils/LoggerUtils';
+const logger = getLogger(__filename);
+
 let client: redis.RedisClient;
 
 class RedisCacheUtils {
   static async init() {
-    return new Promise((resolve) => {
-      logger.info('Initialize Redis Cache');
+    logger.info('Initialize Redis Cache');
+    client = await RedisCacheUtils.getClient();
+  }
 
-      client = redis.createClient(parseInt(process.env.REDIS_PORT), process.env.REDIS_SERVER, {
+  static async getClient(): Promise<redis.RedisClient> {
+    return new Promise((resolve) => {
+      const newClient = redis.createClient(parseInt(process.env.REDIS_PORT), process.env.REDIS_SERVER, {
         auth_pass: process.env.REDIS_PASSWORD,
         tls: { servername: process.env.REDIS_SERVER },
       });
 
-      client.ping(function () {
-        resolve();
+      newClient.ping(function () {
+        resolve(newClient);
       });
     });
   }
@@ -82,8 +88,16 @@ class RedisCacheUtils {
     });
   }
 
+  static async isSetValueExisted(cacheKey: string, cacheVal: string): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      client.sismember(cacheKey, cacheVal, function (err, value) {
+        err ? reject(err) : resolve(value >= 1);
+      });
+    });
+  }
+
   // hash map set
-  static async setHash(cacheKey: string, cacheVal: any): Promise<void> {
+  static async setHash(cacheKey: string, cacheVal: { [key: string]: string }): Promise<void> {
     return new Promise((resolve, reject) => {
       const inputs = [];
       for (const cachePropName of Object.keys(cacheVal)) {
@@ -91,13 +105,37 @@ class RedisCacheUtils {
         inputs.push(cacheVal[cachePropName]);
       }
 
-      client.hmset(cacheKey, ...inputs, function (err, d) {
+      client.hmset(cacheKey, ...inputs, function (err) {
         err ? reject(err) : resolve();
       });
     });
   }
 
-  static async getHash(cacheKey: string): Promise<any> {
+  static async setHashPartial(cacheKey: string, cachePropName: string, cacheVal: number | string): Promise<void> {
+    return new Promise((resolve, reject) => {
+      client.hmset(cacheKey, cachePropName, cacheVal, function (err) {
+        err ? reject(err) : resolve();
+      });
+    });
+  }
+
+  static async delSingleHashProperty(cacheKey: string, cachePropName: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+      client.hdel(cacheKey, cachePropName, function (err) {
+        err ? reject(err) : resolve();
+      });
+    });
+  }
+
+  static async isHashPropertyExisted(cacheKey: string, cachePropName: string): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      client.hexists(cacheKey, cachePropName, function (err, value) {
+        err ? reject(err) : resolve(value >= 1);
+      });
+    });
+  }
+
+  static async getHash(cacheKey: string): Promise<{ [key: string]: string }> {
     return new Promise((resolve, reject) => {
       client.hgetall(cacheKey, function (err, value) {
         err ? reject(err) : resolve(value);
@@ -155,12 +193,29 @@ class InMemoryCacheUtils {
     return memoryCache[cacheKey] || [];
   }
 
+  static async isSetValueExisted(cacheKey: string, cacheVal: string): Promise<boolean> {
+    return (memoryCache[cacheKey] || []).includes(cacheVal);
+  }
+
   // hash map set
-  static async setHash(cacheKey: string, cacheVal: any): Promise<void> {
+  static async setHash(cacheKey: string, cacheVal: { [key: string]: string }): Promise<void> {
     memoryCache[cacheKey] = cacheVal;
   }
 
-  static async getHash(cacheKey: string): Promise<any> {
+  static async setHashPartial(cacheKey: string, cachePropName: string, cacheVal: number | string): Promise<void> {
+    memoryCache[cacheKey] = memoryCache[cacheKey] || {};
+    memoryCache[cacheKey][cachePropName] = cacheVal;
+  }
+
+  static async delSingleHashProperty(cacheKey: string, cachePropName: string): Promise<void> {
+    delete memoryCache[cacheKey][cachePropName];
+  }
+
+  static async isHashPropertyExisted(cacheKey: string, cachePropName: string): Promise<boolean> {
+    return !!memoryCache[cacheKey][cachePropName];
+  }
+
+  static async getHash(cacheKey: string): Promise<{ [key: string]: string }> {
     return memoryCache[cacheKey];
   }
 }
